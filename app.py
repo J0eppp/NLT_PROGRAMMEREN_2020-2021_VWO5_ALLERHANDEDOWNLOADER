cbd1c40e6160c11c3790cba0364468513bd5f2ca
J0eppp <joep.van.dijk04@gmail.com>
2020-09-24 18:26:12 +0200

    Recipes get saved to MySQL database

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import json
+import mysql.connector
 
 f = open('./recipes.txt', 'r')
 urls = [url.replace('\n', '') for url in f.readlines()]
@@ -15,8 +16,11 @@ for url in urls:
 	recipe = {
 		'url': url,
 		'name': name,
+		'imageUrl': '',
 		'ingredients': []
 	}
+	for img in soup.find_all('img', class_='print-only recipe-image'):
+		recipe['imageUrl'] = img.get('src')
 	for li in soup.find_all('li'):
 		if li.get('itemprop') == 'ingredients':
 			ingredient = li.getText().replace('\n', '')
@@ -33,3 +37,15 @@ for url in urls:
 
 with open('./recipes.json', 'w+') as fo:
 	json.dump(recipes, fo, indent=4, ensure_ascii=False)
+
+db = mysql.connector.connect(host = 'localhost', user = 'ahRecipeFinder', password = 'Test123', database = 'ahRecipeFinder')
+for recipe in recipes:
+	cursor = db.cursor()
+	sql = "INSERT INTO recipes (URL, imageURL, name, ingredients) VALUES (%s, %s, %s, %s)"
+	ingredients = recipe['ingredients']
+	i = ''
+	for ingredient in ingredients:
+		i += ingredient + ', '
+	val = (recipe['url'], recipe['imageUrl'], recipe['name'], i)
+	cursor.execute(sql, val)
+	db.commit()
